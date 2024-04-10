775af8ff52f33b5183f92bde243863e191006e05
redpandata19 <19ebe91@gmail.com>
2024-04-09 21:22:19 -0400

    understanding the components to load

--- app.py
+++ app.py
@@ -1,23 +1,84 @@
-# Import SQLAlchemy dependencies
+# Import dependencies
+import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, func
 
-# Create the connection engine
-engine = create_engine("postgresql://username:password@localhost:5432/airport_db")
+from flask import Flask, jsonify
 
-# Create the inspector and connect it to the engine
-inspector = inspect(engine)
+import matplotlib.pyplot as plt
 
-# Collect the names of tables within the database
-tables = inspector.get_table_names()
+import psycopg2
 
-# Print the names of tables within the database
-print("Tables in the database:", tables)
 
-# Using the inspector to print the column names within the 'dow' table and its types
-columns = inspector.get_columns('dow')
-print("Columns in the 'dow' table:")
-for column in columns:
-    print(column["name"], column["type"])
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/airport_db")
+
+# base mapping
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+# Table references
+airports = Base.classes.airports
+states = Base.classes.states
+airlines = Base.classes.airlines
+airport_sizes = Base.classes.airport_sizes
+flights = Base.classes.flights
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/state_airports"
+    )
+
+
+@app.route("/api/v1.0/state_airports")
+def state_airports():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Gather all airports by state"""
+    state_airports = session.query(airports.state_abbrev, func.count()).group_by(airports.state_abbrev).all()
+    session.close()
+    
+    # sort states per airport count
+    sorted_state_airports = sorted(state_airports, key=lambda x: x[1])
+
+    # extract state abbreviations and airport counts
+    states = [result[0] for result in sorted_state_airports]  
+    counts = [result[1] for result in sorted_state_airports]
+
+ # Create a horizontal bar chart
+plt.figure(figsize=(10, 8))  
+plt.barh(states, counts, color='green') 
+
+# create the bar chart and label the axis and title
+plt.xlabel('Number of Airports') 
+plt.ylabel('State') 
+plt.title('Number of Airports by State') 
+plt.tight_layout() 
+# Export chart image
+plt.savefig('images/airports_by_state.png', dpi=300)  
+# Display the chart
+plt.show()
+
+
+
